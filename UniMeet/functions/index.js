78441dff944b5220dd8d5c1165449131b0f01378
MatthewB95 <matt_19_95@live.com.au>
2018-08-04 17:31:08 +1000

    remove message restriction and welcome bot
    removed 12 message restriction and commented out welcome message bot

--- UniMeet/functions/index.js
+++ UniMeet/functions/index.js
@@ -6,7 +6,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Adds a message that welcomes new users into the chat.
+/* // Adds a message that welcomes new users into the chat.
 exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
     console.log('A new user signed in for the first time.');
     const fullName = user.displayName || 'Anonymous';
@@ -19,7 +19,7 @@ exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
       text: `${fullName} signed in for the first time! Welcome!`,
     });
     console.log('Welcome message written to database.');
-  });
+  }); */
 
 // Sends a notifications to all users when a new message is posted.
 exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCreate(
