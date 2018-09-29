3a7533b078b543d465ee00ec64a565a9827aa278
s3602677 <s3602677@student.rmit.edu.au>
2018-09-30 03:26:49 +1000

    Fixed reloading of interest list

--- UniMeet/public/scripts/admin.js
+++ UniMeet/public/scripts/admin.js
@@ -12,41 +12,6 @@ firebase.auth().onAuthStateChanged(function (user) {
         var user = firebase.auth().currentUser;
         var uid = user.uid;
 
-        // Loads list of interests from database and add them to the drop down-lists
-        function loadInterests() {
-
-            // Fade all collections onto page
-            $("#todolist-cont").fadeOut(500);
-
-            var interestsList = document.getElementById("task-list");
-
-            // Remove any existing matches from the page
-            interestsList.innerHTML = "";
-
-            const interestsRef = firestore.doc("interests/interests");
-            interestsRef.get().then(function (doc) {
-                if (doc && doc.exists) {
-                    const interests = doc.data();
-                    console.log("Values: ", Object.values(interests));
-                    console.log("Count: ", Object.values(interests).length);
-
-                    // Add each interest to the list
-                    for (var key in interests) {
-                        $("#task-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + key + "</span>" +
-                         "<div class='btn-group pull-right _task-controls' role='group'>" +
-                         "<button class='btn btn-light _todo-remove' type='button' value=" + key + " onclick='deleteInterest(this.value)'>" + 
-                         "<i class='fa fa-trash'></i></button></div>" +
-                         "<div class='clearfix'></div>"
-                         );
-                    }
-                    // Fade all collections onto page
-                    $("#todolist-cont").fadeIn(1000);
-                }
-            }).catch(function (error) {
-                console.log("Failed to retrieve error: ", error)
-            });
-        }
-
         function addInterest() {
             var newInterest = document.getElementById("newInterest").value.trim();
 
@@ -94,6 +59,38 @@ firebase.auth().onAuthStateChanged(function (user) {
         window.location.replace("/");
     }
 });
+
+// Loads list of interests from database and add them to the drop down-lists
+function loadInterests() {
+    // Fade all collections onto page
+    $("#todolist-cont").fadeOut(500);
+    var interestsList = document.getElementById("task-list");
+    // Remove any existing matches from the page
+    interestsList.innerHTML = "";
+    
+    const interestsRef = firestore.doc("interests/interests");
+    interestsRef.get().then(function (doc) {
+        if (doc && doc.exists) {
+            const interests = doc.data();
+            console.log("Values: ", Object.values(interests));
+            console.log("Count: ", Object.values(interests).length);
+            // Add each interest to the list
+            for (var key in interests) {
+                $("#task-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + key + "</span>" +
+                 "<div class='btn-group pull-right _task-controls' role='group'>" +
+                 "<button class='btn btn-light _todo-remove' type='button' value=" + key + " onclick='deleteInterest(this.value)'>" + 
+                 "<i class='fa fa-trash'></i></button></div>" +
+                 "<div class='clearfix'></div>"
+                 );
+            }
+            // Fade all collections onto page
+            $("#todolist-cont").fadeIn(1000);
+        }
+    }).catch(function (error) {
+        console.log("Failed to retrieve error: ", error)
+    });
+}
+
 
 function deleteInterest(interest) {
 
