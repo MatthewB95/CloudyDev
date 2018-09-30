a102334c5df9a9000092415b608c88d46cd4df50
s3602677 <s3602677@student.rmit.edu.au>
2018-09-30 16:59:17 +1000

    Changed admin page to have navigation tabs, Add/removal functionality for admin accounts

--- UniMeet/public/scripts/admin.js
+++ UniMeet/public/scripts/admin.js
@@ -9,15 +9,28 @@ firestore.settings(settings);
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
+        loadAdmins();
         loadInterests();
 
         // Add button listeners
+        document.getElementById("newAdminButton").addEventListener("click", addAdmin);
+        document.getElementById("adminRefreshButton").addEventListener("click", loadAdmins);
+        
         document.getElementById("newInterestButton").addEventListener("click", addInterest);
-        document.getElementById("refreshButton").addEventListener("click", loadInterests);
+        document.getElementById("interestRefreshButton").addEventListener("click", loadInterests);
+
+        // Add new admin when the ENTER key is hit
+        var adminField = document.getElementById("newAdmin");
+        adminField.addEventListener("keyup", function(event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                document.getElementById("newAdminButton").click();
+            }
+        });
 
         // Add new interest when the ENTER key is hit
-        var inputField = document.getElementById("newInterest");
-        inputField.addEventListener("keyup", function(event) {
+        var interestField = document.getElementById("newInterest");
+        interestField.addEventListener("keyup", function(event) {
             event.preventDefault();
             if (event.keyCode === 13) {
                 document.getElementById("newInterestButton").click();
@@ -29,11 +42,112 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
+////////////////////////////   ADMIN   //////////////////////////////////
+// Loads list of admins from database and add them to the drop down-lists
+function loadAdmins() {
+    // Fade all collections onto page
+    $("#adminList-cont").fadeOut(500);
+    var adminsList = document.getElementById("admin-list");
+    // Remove any existing matches from the page
+    adminsList.innerHTML = "";
+
+    firestore.collection("admin").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function(admin) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(admin.id, " => ", admin.data());
+
+        $("#admin-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + admin.id + "</span>" +
+        "<div class='btn-group pull-right _task-controls' role='group'>" +
+        "<button class='btn btn-light _todo-remove' type='button' value=" + admin.id + " onclick='deleteAdmin(this.value)'>" + 
+        "<i class='fa fa-trash'></i></button></div>" +
+        "<div class='clearfix'></div>"
+        );
+        });
+        $("#adminList-cont").fadeIn(1000);
+    });
+}
+
+function addAdmin() {
+
+    var user = firebase.auth().currentUser;
+
+    var newAdmin = document.getElementById("newAdmin").value.trim();
+    document.getElementById("newAdmin").value = "";
+    // Return if the new admin is an empty field
+    if (newAdmin == "" || newAdmin == null) {
+        return;
+    }
+
+    var adminAdd = firebase.functions().httpsCallable('adminAdd');
+    console.log("FIREBASE: Adding admin: " + newAdmin);
+
+    const data = {
+        uid     : user.uid,
+        command : 1,
+        item    : newAdmin,
+        subItem : 1 // Privilege level
+    };
+    adminAdd(data).then(function(result) {
+        console.log("FIREBASE: Successfully added admin.");
+        // Refresh admins page
+        loadAdmins();
+    }).catch(function(error) {
+        console.error("FIREBASE: Failed to add admin.", error);
+    });
+}
+
+
+function deleteAdmin(admin) {
+
+    var user = firebase.auth().currentUser;
+
+    var adminRemove = firebase.functions().httpsCallable('adminRemove');
+    console.log("FIREBASE: Removing Admin: " + admin);
+
+    const data = {
+        uid     : user.uid,
+        command : 1,
+        item    : admin,
+        subItem : null
+    };
+
+    adminRemove(data).then(function(result) {
+        console.log("FIREBASE: Successfully removed admin.");
+        // Refresh admins page
+        loadAdmins();
+    }).catch(function(error) {
+        console.error("FIREBASE: Failed to remove admin.", error);
+    });
+}
+
+
+////////////////////////////   ADMIN   //////////////////////////////////
+
+
+
+
+
+
+////////////////////////////   UNIVERSITY   //////////////////////////////////
+
+
+/////////////////////////////   COURSES  ////////////////////////////////////
+
+
+
+
+
+/////////////////////////////   COURSES  ////////////////////////////////////
+
+
+
+////////////////////////////  INTERESTS  ///////////////////////////////////
+
 // Loads list of interests from database and add them to the drop down-lists
 function loadInterests() {
     // Fade all collections onto page
-    $("#todolist-cont").fadeOut(500);
-    var interestsList = document.getElementById("task-list");
+    $("#interestList-cont").fadeOut(500);
+    var interestsList = document.getElementById("interest-list");
     // Remove any existing matches from the page
     interestsList.innerHTML = "";
 
@@ -45,7 +159,7 @@ function loadInterests() {
             console.log("Count: ", Object.values(interests).length);
             // Add each interest to the list
             for (var key in interests) {
-                $("#task-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + key + "</span>" +
+                $("#interest-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + key + "</span>" +
                  "<div class='btn-group pull-right _task-controls' role='group'>" +
                  "<button class='btn btn-light _todo-remove' type='button' value=" + key + " onclick='deleteInterest(this.value)'>" + 
                  "<i class='fa fa-trash'></i></button></div>" +
@@ -53,7 +167,7 @@ function loadInterests() {
                  );
             }
             // Fade all collections onto page
-            $("#todolist-cont").fadeIn(1000);
+            $("#interestList-cont").fadeIn(1000);
         }
     }).catch(function (error) {
         console.log("Failed to retrieve error: ", error)
@@ -112,3 +226,4 @@ function deleteInterest(interest) {
         console.error("FIREBASE: Failed to remove interest.", error);
     });
 }
+////////////////////////////  INTERESTS  ///////////////////////////////////
