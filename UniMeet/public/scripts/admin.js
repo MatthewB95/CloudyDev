3b4813647d1907c7f777c1404e1ec8b16094cb18
s3602677 <s3602677@student.rmit.edu.au>
2018-09-30 21:36:45 +1000

    Navigation, add/delete universities, Fix to deleting items with spaces in their names

--- UniMeet/public/scripts/admin.js
+++ UniMeet/public/scripts/admin.js
@@ -10,14 +10,20 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
         loadAdmins();
+        loadUniversities();
         loadInterests();
 
         // Add button listeners
         document.getElementById("newAdminButton").addEventListener("click", addAdmin);
         document.getElementById("adminRefreshButton").addEventListener("click", loadAdmins);
+
+        document.getElementById("newUniversityButton").addEventListener("click", addUniversity);
+        document.getElementById("universityRefreshButton").addEventListener("click", loadUniversities);
         
         document.getElementById("newInterestButton").addEventListener("click", addInterest);
         document.getElementById("interestRefreshButton").addEventListener("click", loadInterests);
+
+        document.getElementById("logOutButton").addEventListener("click", logOut);
 
         // Add new admin when the ENTER key is hit
         var adminField = document.getElementById("newAdmin");
@@ -25,6 +31,15 @@ firebase.auth().onAuthStateChanged(function (user) {
             event.preventDefault();
             if (event.keyCode === 13) {
                 document.getElementById("newAdminButton").click();
+            }
+        });
+
+        // Add new university when the ENTER key is hit
+        var universityField = document.getElementById("newUniversity");
+        universityField.addEventListener("keyup", function(event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                document.getElementById("newUniversityButton").click();
             }
         });
 
@@ -36,6 +51,11 @@ firebase.auth().onAuthStateChanged(function (user) {
                 document.getElementById("newInterestButton").click();
             }
         });
+
+        function logOut() {
+            firebase.auth().signOut();
+            window.location.replace("/");
+        }
 
     } else {
         window.location.replace("/");
@@ -58,7 +78,7 @@ function loadAdmins() {
 
         $("#admin-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + admin.id + "</span>" +
         "<div class='btn-group pull-right _task-controls' role='group'>" +
-        "<button class='btn btn-light _todo-remove' type='button' value=" + admin.id + " onclick='deleteAdmin(this.value)'>" + 
+        "<button class='btn btn-light _todo-remove' type='button' value='" + admin.id + "' onclick='deleteAdmin(this.value)'>" + 
         "<i class='fa fa-trash'></i></button></div>" +
         "<div class='clearfix'></div>"
         );
@@ -119,16 +139,89 @@ function deleteAdmin(admin) {
         console.error("FIREBASE: Failed to remove admin.", error);
     });
 }
-
-
 ////////////////////////////   ADMIN   //////////////////////////////////
 
+////////////////////////////   UNIVERSITY   //////////////////////////////////
+// Loads list of universities from database and add them to the drop down-lists
+function loadUniversities() {
+    // Fade all collections onto page
+    $("#universityList-cont").fadeOut(500);
+    var universitiesList = document.getElementById("university-list");
+    // Remove any existing matches from the page
+    universitiesList.innerHTML = "";
+
+    firestore.collection("degree").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function(universities) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(universities.id, " => ", universities.data());
+
+        $("#university-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + universities.id + "</span>" +
+        "<div class='btn-group pull-right _task-controls' role='group'>" +
+        "<button class='btn btn-light _todo-remove' type='button' value='" + universities.id + "' onclick='deleteUniversity(this.value)'>" + 
+        "<i class='fa fa-trash'></i></button></div>" +
+        "<div class='clearfix'></div>"
+        );
+        });
+        $("#universityList-cont").fadeIn(1000);
+    });
+}
+
+function addUniversity() {
+
+    var user = firebase.auth().currentUser;
+
+    var newUniversity = document.getElementById("newUniversity").value.trim();
+    document.getElementById("newUniversity").value = "";
+    // Return if the new university is an empty field
+    if (newUniversity == "" || newUniversity == null) {
+        return;
+    }
+
+    var adminAdd = firebase.functions().httpsCallable('adminAdd');
+    console.log("FIREBASE: Adding university: " + newUniversity);
+
+    const data = {
+        uid     : user.uid,
+        command : 2,
+        item    : newUniversity,
+        subItem : null // Privilege level
+    };
+    adminAdd(data).then(function(result) {
+        console.log("FIREBASE: Successfully added university.");
+        // Refresh universities page
+        loadUniversities();
+    }).catch(function(error) {
+        console.error("FIREBASE: Failed to add university.", error);
+    });
+}
 
 
+function deleteUniversity(university) {
 
+    var user = firebase.auth().currentUser;
 
+    var adminRemove = firebase.functions().httpsCallable('adminRemove');
+    console.log("FIREBASE: Removing university: " + university);
+
+    const data = {
+        uid     : user.uid,
+        command : 2,
+        item    : university,
+        subItem : null
+    };
+
+    adminRemove(data).then(function(result) {
+        console.log("FIREBASE: Successfully removed university.");
+        // Refresh universities page
+        loadUniversities();
+    }).catch(function(error) {
+        console.error("FIREBASE: Failed to remove university.", error);
+    });
+}
 
 ////////////////////////////   UNIVERSITY   //////////////////////////////////
+
+
 
 
 /////////////////////////////   COURSES  ////////////////////////////////////
@@ -161,7 +254,7 @@ function loadInterests() {
             for (var key in interests) {
                 $("#interest-list").append("<li class='list-group-item _task'><span class='_todo-text'>" + key + "</span>" +
                  "<div class='btn-group pull-right _task-controls' role='group'>" +
-                 "<button class='btn btn-light _todo-remove' type='button' value=" + key + " onclick='deleteInterest(this.value)'>" + 
+                 "<button class='btn btn-light _todo-remove' type='button' value='" + key + "' onclick='deleteInterest(this.value)'>" + 
                  "<i class='fa fa-trash'></i></button></div>" +
                  "<div class='clearfix'></div>"
                  );
